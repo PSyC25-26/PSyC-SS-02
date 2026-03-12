9094a80ea12ad6dde2a947be6acb859272d4fad0
gonzatorra <haizea.g@opendeusto.es>
2026-03-12 19:19:19 +0100

    Proyecto creado (dependecia JPA no funciona porque requiere BBDD, cuando empecemos descomentarlo)

---
+++ banca-online/src/main/java/es/deusto/banca_online/BancaOnlineApplication.java
@@ -0,0 +1,13 @@
+package es.deusto.banca_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancaOnlineApplication.class, args);
+	}
+
+}
