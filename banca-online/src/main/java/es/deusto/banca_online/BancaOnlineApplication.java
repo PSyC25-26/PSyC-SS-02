562fc2d89f3820860eec4279217a7e29daa8c640
noraib <nora.ibarguren@opendeusto.es>
2026-03-13 11:33:05 +0100

    Instalacion Nora y cambio menor en BancaOnlineApplication.java

--- banca-online/src/main/java/es/deusto/banca_online/BancaOnlineApplication.java
+++ banca-online/src/main/java/es/deusto/banca_online/BancaOnlineApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BancaOnlineApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(BancaOnlineApplication.class, args);
 	}
 
