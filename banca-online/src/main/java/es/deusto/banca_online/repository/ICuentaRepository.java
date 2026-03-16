be850c2fededc1afe59f4abb2178aed16d640923
enekobarod <144792607+enekobarod@users.noreply.github.com>
2026-03-16 20:40:06 +0100

    Repositorio CuentaRepository creado

---
+++ banca-online/src/main/java/es/deusto/banca_online/repository/ICuentaRepository.java
@@ -0,0 +1,15 @@
+package es.deusto.banca_online.repository;
+
+import es.deusto.banca_online.entity.Cuenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ICuentaRepository extends JpaRepository<Cuenta, Long> {
+    Optional<Cuenta> findByNumeroCuenta(String numeroCuenta);
+    List<Cuenta> findByClienteId(Long clienteId);
+    boolean existsByNumeroCuenta(String numeroCuenta);
+}
