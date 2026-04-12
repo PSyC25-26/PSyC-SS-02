35f0f743795f5a33b176778565d835116fa7caee
Imanol-U <imanol.u@opendeusto.es>
2026-04-12 19:14:33 +0200

    Creación inicializador datos admin

---
+++ banca-online/src/main/java/es/deusto/banca_online/config/DataInitializer.java
@@ -0,0 +1,34 @@
+package es.deusto.banca_online.config;
+
+import es.deusto.banca_online.entity.ERol;
+import es.deusto.banca_online.entity.Usuario;
+import es.deusto.banca_online.repository.IUsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final IUsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public DataInitializer(IUsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) {
+        if (!usuarioRepository.existsByEmail("admin@banco.com")) {
+            Usuario admin = new Usuario();
+            admin.setEmail("admin@banco.com");
+            admin.setPassword(passwordEncoder.encode("admin123"));
+            admin.setRol(ERol.ADMIN);
+            admin.setActivo(true);
+            admin.setClienteId(null);
+            usuarioRepository.save(admin);
+            System.out.println("Usuario admin creado");
+        }
+    }
+}
