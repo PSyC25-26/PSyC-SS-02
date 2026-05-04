6dffec066d66ad597c2ab635e5773c8b803ee437
gonzatorra <haizea.g@opendeusto.es>
2026-05-04 17:02:56 +0200

    feat: actualizar repositorio para buscar transacciones por origen o destino

--- banca-online/src/main/java/es/deusto/banca_online/repository/ITransaccionRepository.java
+++ banca-online/src/main/java/es/deusto/banca_online/repository/ITransaccionRepository.java
@@ -2,7 +2,9 @@ package es.deusto.banca_online.repository;
 import es.deusto.banca_online.entity.Transaccion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ITransaccionRepository extends JpaRepository<Transaccion, Long> {
+    List<Transaccion> findByCuentaOrigenIdOrCuentaDestinoIdOrderByFechaDesc(Long origenId, Long destinoId);
 }
